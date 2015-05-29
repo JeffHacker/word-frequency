13e4c69cde4e9608272b2c3e0a4219f2052bbabc
Jeff Hacker <reachjkh@yahoo.com>
2015-05-28 20:27:26 -0400

    put logic in func Analyze

--- word_frequency.py
+++ word_frequency.py
@@ -3,16 +3,19 @@ import re
 with open('sample.txt', ) as temp_file:
     text_to_analyze = temp_file.read()
     text_to_analyze = re.sub(r'[^A-Za-z\s]', "", text_to_analyze.lower())
+    list_to_analyze = text_to_analyze.split()
     print(text_to_analyze)
 
 
 histogram = {}
 
-for word in text_to_analyze:
-    if word in histogram:
-        histogram[word] += 1
-    else:
-        histogram[word] = 1
+def analyze ():
+    for word in list_to_analyze:
+        if word in histogram:
+            histogram[word] += 1
+        else:
+            histogram[word] = 1
 
 
+analyze()
 print(histogram)
