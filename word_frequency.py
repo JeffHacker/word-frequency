b8501a6a0f3f33af28f2424b91cb58ad1c22fe57
Jeff Hacker <reachjkh@yahoo.com>
2015-05-28 21:16:22 -0400

    Sorted by Key so far

--- word_frequency.py
+++ word_frequency.py
@@ -1,21 +1,33 @@
 import re
+# import collections # Google search
 
 with open('sample.txt', ) as temp_file:
     text_to_analyze = temp_file.read()
-    text_to_analyze = re.sub(r'[^A-Za-z\s]', "", text_to_analyze.lower())
+    text_to_analyze = re.sub(r'[^A-Za-z\s]', "", text_to_analyze.lower()) #got help on the \s Sorren
     list_to_analyze = text_to_analyze.split()
-    print(text_to_analyze)
+    #  print(text_to_analyze)
 
 
 histogram = {}
 
-def analyze ():
-    for word in list_to_analyze:
-        if word in histogram:
-            histogram[word] += 1
-        else:
-            histogram[word] = 1
+
+for word in list_to_analyze: # class work
+    if word in histogram:
+        histogram[word] += 1
+    else:
+        histogram[word] = 1
 
 
-analyze()
-print(histogram)
+# list(histogram.items())
+# print(histogram)
+# histogram = sorted((v, k) for k, v in histogram.items()) # Google search
+#  print(histogram)
+
+
+for key in sorted(histogram):
+	print(key, ':', histogram[key])
+
+
+# analyze()
+# sorted_list()
+# print(histogram)
