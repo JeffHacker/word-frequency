5e2bfa90e29d9e6295c8e0754c77abefbbdfbfb2
Jeff Hacker <reachjkh@yahoo.com>
2015-05-31 11:04:30 -0400

    Cleaned out some bad ideas

--- word_frequency.py
+++ word_frequency.py
@@ -3,34 +3,18 @@ import re
 
 with open('sample.txt', ) as temp_file:
     text_to_analyze = temp_file.read()
-    text_to_analyze = re.sub(r'[^A-Za-z\s]', "", text_to_analyze.lower()) #got help on the \s Sorren
+    # got help on the \s from Soren
+    text_to_analyze = re.sub(r'[^A-Za-z\s]', "", text_to_analyze.lower())
     list_to_analyze = text_to_analyze.split()
     #  print(text_to_analyze)
 
 
 histogram = {}
-
-
-
-for word in list_to_analyze: # class work
+for word in list_to_analyze:  # class work
     if word in histogram:
         histogram[word] += 1
     else:
         histogram[word] = 1
 
-# print(histogram)
-
-
-
-# list(histogram.items())
-# print(histogram)
-# histogram = reversed(sorted((v, k) for k, v in histogram.items())) # Google search modded by another Google search
-# print(histogram)
-
-
-# for key in reversed(sorted(histogram)):
-#	print(key, ':', histogram[key])
-
-
-# sorted_list()
-# print(histogram)
+histogram_list = histogram.items()
+print(histogram_list)
