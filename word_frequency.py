f3ef621556c710859b052398ed193b825b251728
Jeff Hacker <reachjkh@yahoo.com>
2015-05-31 20:23:05 -0400

    Added an 'ignore list' and some formatting

--- word_frequency.py
+++ word_frequency.py
@@ -13,7 +13,7 @@ print("the_young_explorer.rtf")
 print("phil_the_fiddler.rtf")
 print("war_and_peace.rtf")
 print("------------")
-file_to_analyze = input("Which do you choose?")
+file_to_analyze = input("Which do you choose?  ")
 
 with open(file_to_analyze, ) as temp_file:
     text_to_analyze = temp_file.read()
@@ -25,11 +25,31 @@ with open(file_to_analyze, ) as temp_file:
 
 def word_frequency(list_to_analyze):
     histogram = {}
+    ignore_list = ('a', 'able', 'about', 'across', 'after', 'all', 'almost',
+                   'also', 'am', 'among', 'an', 'and', 'any', 'are', 'as',
+                   'at', 'be', 'because', 'been', 'but', 'by', 'can', 'cannot',
+                   'could', 'dear', 'did', 'do', 'does', 'either', 'else',
+                   'ever', 'every', 'for', 'from', 'get', 'got', 'had', 'has',
+                   'have', 'he', 'her', 'hers', 'him', 'his', 'how', 'however',
+                   'i', 'if', 'in', 'into', 'is', 'it', 'its', 'just', 'least',
+                   'let', 'like', 'likely', 'may', 'me', 'might', 'most',
+                   'must', 'my', 'neither', 'no', 'nor', 'not', 'of', 'off',
+                   'often', 'on', 'only', 'or', 'other', 'our', 'own',
+                   'rather', 'said', 'say', 'says', 'she', 'should', 'since',
+                   'so', 'some', 'than', 'that', 'the', 'their', 'them',
+                   'then', 'there', 'these', 'they', 'this', 'tis', 'to',
+                   'too', 'twas', 'us', 'wants', 'was', 'we', 'were', 'what',
+                   'when', 'where', 'which', 'while', 'who', 'whom', 'why',
+                   'will', 'with', 'would', 'yet', 'you', 'your')
+
     for word in list_to_analyze:  # class work
-        if word in histogram:
-            histogram[word] += 1
+        if word in ignore_list:
+            pass
         else:
-            histogram[word] = 1
+            if word in histogram:
+                histogram[word] += 1
+            else:
+                histogram[word] = 1
 #   print(histogram)
     return histogram
 
@@ -37,8 +57,6 @@ histogram = word_frequency(list_to_analyze)
 # print(type(histogram))
 # def sorted_by_val():
 sorted_by_val = sorted(histogram.items(), key=lambda t: t[1], reverse=True)
-
-# _____________
 
 hash_a, hash_b = sorted_by_val[0]
 hash_mult = 50 / int(hash_b)
