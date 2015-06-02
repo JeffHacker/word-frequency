307e1ef2fce7c409df32eac481d8ea45d263f922
Jeff Hacker <reachjkh@yahoo.com>
2015-06-02 14:45:27 -0400

    Joel made some tweaks for me

--- word_frequency.py
+++ word_frequency.py
@@ -15,7 +15,7 @@ print("war_and_peace.rtf")
 print("------------")
 file_to_analyze = input("Which do you choose?  ")
 
-with open(file_to_analyze, ) as temp_file:
+with open(file_to_analyze) as temp_file:
     text_to_analyze = temp_file.read()
     # got help on the \s from Soren
     text_to_analyze = re.sub(r'[^A-Za-z\s]', "", text_to_analyze.lower())
@@ -43,13 +43,19 @@ def word_frequency(list_to_analyze):
                    'will', 'with', 'would', 'yet', 'you', 'your')
 
     for word in list_to_analyze:  # class work
-        if word in ignore_list:
+        if wordin ignore_list:
             pass
         else:
             if word in histogram:
                 histogram[word] += 1
             else:
                 histogram[word] = 1
+
+        # if word not in ignore_list:
+        #     if word in histogram:
+        #         histogram[word] += 1
+        #     else:
+        #         histogram[word] = 1
 #   print(histogram)
     return histogram
 
@@ -58,7 +64,7 @@ histogram = word_frequency(list_to_analyze)
 # def sorted_by_val():
 sorted_by_val = sorted(histogram.items(), key=lambda t: t[1], reverse=True)
 
-hash_a, hash_b = sorted_by_val[0]
+_, hash_b = sorted_by_val[0]
 hash_mult = 50 / int(hash_b)
 # print("hash mult is {}".format(hash_mult))
 
